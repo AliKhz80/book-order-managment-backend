6185e1d0679096198384a04b5647248a54960014
Ali <AliKhanzadeh98@gmail.com>
2026-08-10 18:09:49 +0330

    hot fix

--- CatalogService/Endpoints/BookEndpoints.cs
+++ CatalogService/Endpoints/BookEndpoints.cs
@@ -1,11 +1,10 @@
-using CatalogService.Application.Extentions.Models;
-using CatalogService.Application.Features.Book.Commands.DeleteBook;
-using CatalogService.Application.Features.Book.Queries.GetBookModelById;
-using CatalogService.Application.Features.Book.Queries.GetBooksByFilter;
+using CatalogService.Application.Common.QueryModels;
+using CatalogService.Application.UseCases.Book.Commands.DeleteBook;
+using CatalogService.Application.UseCases.Book.Queries.GetBookModelById;
+using CatalogService.Application.UseCases.Book.Queries.GetBooksByFilter;
 using CatalogService.Application.UseCases.Book.Commands.AddBook;
 using CatalogService.Application.UseCases.Book.Commands.UpdateBook;
 using CatalogService.Application.UseCases.Book.ViewModels;
-using CatalogService.Domain.Enums;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
